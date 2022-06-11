287237ba4e004849f408bcef5460af0285427fda
Miguel-A-Jara <0miguel.jara15@gmail.com>
2022-06-11 16:07:17 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				/*	Solution 1	*/
+				Console.Write(i + " ");
+
+				if (i % 3 == 0)
+				{
+					Console.Write("Fizz");
+				}
+
+				if (i % 5 == 0)
+				{
+					Console.Write("Buzz");
+				}
+
+				Console.WriteLine();
+
+				/*	Solution 2	*/
+				Console.WriteLine($"{i} {(i % 3 == 0 ? "Fizz" : "")}{(i % 5 == 0 ? "Buzz" : "")}");
+			}
+		}
+	}
+}
